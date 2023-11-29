1a8f95ca3a8e456586f7a9770be109e2fa1cfeb3
Hjordten <andr314e@stud.kea.dk>
2023-11-29 19:14:33 +0100

    .

---
+++ src/main/java/com/example/bookhavenback/azuretest/StudentRestController.java
@@ -0,0 +1,33 @@
+package com.example.bookhavenback.azuretest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StudentRestController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping("/")
+    public String detteErRoden() {
+        return "Du er i roden af JPAStudent22";
+    }
+
+    @GetMapping("/students")
+    public List<Student> listStudents() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println("Indsætter ny student");
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+}
