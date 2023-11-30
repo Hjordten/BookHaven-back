60b0c7c64d8d78100bd0d6911d49732c70015a50
Hjordten <andr314e@stud.kea.dk>
2023-11-30 20:28:03 +0100

    .

--- src/main/java/com/example/bookhavenback/admin/model/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookhavenback.admin.model;
-
-public class Admin {
-
-
-
-
-}
---
+++ src/main/java/com/example/bookhavenback/visitor/controller/Restcontroller.java
@@ -0,0 +1,13 @@
+package com.example.bookhavenback.visitor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Restcontroller {
+
+    @GetMapping("/test")
+    public String test (){
+        return "this is a test";
+    }
+}
