c356e4b46237d5ee3e6db85dda979f7817c8c730
Hjordten <andr314e@stud.kea.dk>
2023-12-01 12:42:39 +0100

    .

--- src/main/java/com/example/bookhavenback/controller/Restcontroller.java
+++ src/main/java/com/example/bookhavenback/controller/Restcontroller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Restcontroller {
 
+
     @GetMapping("/test")
     public String test (){
         return "this is a test";
