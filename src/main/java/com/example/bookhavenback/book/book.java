fafa0eaa70ee6fe7fad115890b4b363841ec3b13
Hjordten <andr314e@stud.kea.dk>
2023-12-01 11:21:26 +0100

    .

---
+++ src/main/java/com/example/bookhavenback/book/book.java
@@ -0,0 +1,16 @@
+package com.example.bookhavenback.book;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class book {
+
+
+    private String name;
+    private String genre;
+}
--- src/main/java/com/example/bookhavenback/controller/Restcontroller.java
+++ src/main/java/com/example/bookhavenback/controller/Restcontroller.java
@@ -1,4 +1,4 @@
-package com.example.bookhavenback.visitor.controller;
+package com.example.bookhavenback.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
