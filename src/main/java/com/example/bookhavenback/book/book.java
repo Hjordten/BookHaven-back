3e31b7a1839602badfe097b138804011f427fc68
Hjordten <andr314e@stud.kea.dk>
2023-12-07 11:42:39 +0100

    .

--- src/main/java/com/example/bookhavenback/book/book.java
+++ src/main/java/com/example/bookhavenback/book/book.java
@@ -1,18 +1,20 @@
 package com.example.bookhavenback.book;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity @Getter @Setter @NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class book {
 
 
     @Id
-    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private String genre;
 
--- src/main/java/com/example/bookhavenback/controller/Restcontroller.java
+++ src/main/java/com/example/bookhavenback/controller/Restcontroller.java
@@ -1,7 +1,11 @@
 package com.example.bookhavenback.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
 
 @RestController
 public class Restcontroller {
@@ -11,5 +15,13 @@ public class Restcontroller {
     @GetMapping("/test")
     public String test (){
         return "this is a test";
+    }
+
+    @PostMapping("/api/test")
+    public String testPost(@RequestBody Book book) {
+        // Assuming you have a service layer to handle the book creation and persistence
+        // For simplicity, we'll just print the received book details
+        System.out.println("Received book: " + book.toString());
+        return "Book received successfully";
     }
 }
